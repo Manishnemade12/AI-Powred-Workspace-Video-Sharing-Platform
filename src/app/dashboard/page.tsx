8683c2302906c97f63396b1ae7af5326825ed898
Manish nemade <mnemade140@gmail.com>
2024-11-16 21:49:08 +0530

    developed auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,9 +7,8 @@ type Props = {}
 const Dashboardpage = async (props: Props) => {
   const auth = await OnAuthenticateUser();
 
-  if (auth.status === 200 || auth.status === 201) {
-    return redirect(`/dashboard/${auth.user?.workspace[0].id}`);
-  }
+  if (auth.status === 200 || auth.status === 201)
+    return redirect(`/dashboard/${auth.user?.workspace[0].id}`)
 
   if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
     return redirect('/auth/sign-in');
